04ab013b55ea002c432ca665d01fc6cb9cb7fc7f
امیرحسین عنبری <a.anbari@greenweb.local>
2021-11-03 15:03:39 +0330

    first commit

---
+++ src/routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+/** @var \Laravel\Lumen\Routing\Router $router */
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+use App\Models\User;
+
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
+
+
+
+
+$router->group([
+    'prefix' => 'auth'
+], function ($router) {
+    $router->post('login', 'AuthController@login');
+    $router->post('logout', 'AuthController@logout');
+    $router->post('refresh', 'AuthController@refresh');
+    $router->post('me', 'AuthController@me');
+});
